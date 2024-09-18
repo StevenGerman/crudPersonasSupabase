b2cf6fafa50abd74dbbe0eba44724932002e23cd
StevenGerman <germansteven2024@gmail.com>
2024-09-18 19:50:02 -0300

    metodo get personas

--- src/app/components/persona/persona.component.ts
+++ src/app/components/persona/persona.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { PersonasService } from 'src/app/services/personas.service';
+import { Persona } from 'src/app/data-model/persona.model';
+import { Observable,delay,timeout } from 'rxjs';
+
 
 @Component({
   selector: 'app-persona',
@@ -6,5 +11,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./persona.component.css']
 })
 export class PersonaComponent {
+
+  listaPersonas: Persona[] | undefined;
+
+  idEditar : number = 0;
+
+  ngOnInit():void{
+    this.getAllPersonas();
+  }
+
+  verFormulario: boolean = false;
+  nuevo: boolean = true;
+
+  formPersona = this.formB.group({
+    perDni: [''],
+    perNombreApellido: [''],
+    perEmail: [''],
+    perFechaNacimiento: ['']
+  })
+
+  constructor(
+    private formB : FormBuilder,
+    private servicioPersonas: PersonasService
+  ){}
+
+
+
+
+  getAllPersonas() {
+    this.servicioPersonas.getAll().subscribe((res)=> this.listaPersonas = res)
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
